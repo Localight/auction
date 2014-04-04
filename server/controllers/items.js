63aa5e3c1663d2b752192867aa7651ae5e9125bc
Zlatko Đurić <zladuric@gmail.com>
2014-04-04 07:22:33 +0200

    Fixes for the email templates

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,6 +1,6 @@
 var Item = require('../models/items');
 exports.get = function(req, res) {
-    Item.find({}, function(err, items) {
+    Item.find({}, 'studentNumber itemNumber image', function(err, items) {
         if(err) {
             console.log('Error getting items: ', err);
             return res.json(500, err);
