f854a73065951692a08757e260706b50f22b9a60
Zlatko Đurić <zladuric@gmail.com>
2014-04-12 14:23:49 +0200

    added timestamp and last bid for items endpoint

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,11 +1,65 @@
 var Item = require('../models/items');
+var Bid = require('../models/bids');
 exports.get = function(req, res) {
-    Item.find({}, 'studentNumber itemNumber image', function(err, items) {
+    Item.find({}, 'studentNumber itemNumber image')
+    .lean()
+    .exec(function(err, items) {
         if(err) {
             console.log('Error getting items: ', err);
             return res.json(500, err);
         }
-        res.json(items);
+        if(!items.length) {
+            return res.json([]);
+        }
+        var bidIds = [];
+        for (var i in items) {
+            bidIds.push(items[i].itemNumber);
+        }
+        Bid.find({
+            item: {
+                $in: bidIds
+            }
+        })
+        .sort('item')
+        .exec(function(err, bids){
+            console.log('items: ', items.length, '\r\nbids: ', bids.length)
+            if(err) {
+                return res.json(500, {message: 'Error'});
+            }
+            for(var i in items) {
+                var itemBids = bids.filter(function(bid) {
+                    return items[i].itemNumber == bid.item;
+                });
+                if(!itemBids.length) {
+                    items[i].lastBid = {};
+                    items[i].timestamp = null;
+                    continue;
+                }
+                var bid = itemBids.shift();
+                while (bid !== undefined) {
+                    if(!items[i].lastBid) {
+                        items[i].lastBid = {
+                            value: bid.bid
+                            , timestamp: bid.timestamp
+                        };
+                        items[i].timestamp = bid.timestamp;
+                        bid = itemBids.shift();
+                        continue;
+                    }
+                    if(parseFloat(items[i].lastBid.value) > parseFloat(bid.bid)) {
+                        bid = itemBids.shift();
+                        continue;
+                    }
+                    items[i].timestamp = bid.timestamp;
+                    items[i].lastBid = {
+                        value: bid.bid
+                        , timestamp: bid.timestamp
+                    }
+                    bid = itemBids.shift();
+                }
+            };
+            return res.json(items);
+        });
     });
 };
 
@@ -20,3 +74,29 @@ exports.getItemByNumber = function(req, res) {
     });
 };
 
+
+
+function checkHighestBidder(bids, data) {
+    var amount = parseFloat(data.amount);
+    for (var i in bids) {
+        if(parseFloat(bids[i].bid) >= amount) {
+            return false;
+        }
+    }
+    return true;
+}
+function getHighestBid(bids) {
+    var amount = 0;
+    var idx;
+    for (var i in bids) {
+        if(parseFloat(bids[i].bid) > amount) {
+            amount = parseFloat(bids[i].bid);
+            idx = i;
+        }
+    }
+    if(amount > 0) {
+        return bids[idx];
+    } else {
+        return null;
+    }
+}
